3e1c2cb6132687f56ac762413dcd842d31caa40e
Tuhin-ninja <157907780+Tuhin-ninja@users.noreply.github.com>
2024-06-12 10:05:32 +0600

    problem8 solved

---
+++ problem-8.ts
@@ -0,0 +1,16 @@
+{
+
+class Car{
+    constructor(public brand : string, public model : string, public year : number){}
+    displayInfo(){
+        console.log(`Your car model is: ${this.year} ${this.brand} ${this.model}`)
+    }
+}
+
+const car = new Car('Toyota','Corolla',2020) 
+car.displayInfo()
+
+
+
+
+}
